d61d8ec9bc0e29476fe2146fd77c495ab988ac60
Santiago Trini <santiagotrini@gmail.com>
2019-07-01 17:12:14 -0300

    introduce bug

--- parque.js
+++ parque.js
@@ -1,14 +1,20 @@
 let btnAgregar = document.getElementById('btn-1'); // guardo el elemento HTML en una variable
 let btnFinalizar = document.getElementById('btn-2');
 
+let clienteMin = 9999;
+let clienteMax = 0;
+
 // arrays vacios
-let entradas = [];
-let compra = [];
+let entradas = []; // array para todas las ventas
+let compra = [];  // array para una compra
 
 btnAgregar.onclick = function() {
   let entrada = document.getElementById('tf').value;
+  console.log(entrada);
   compra.push(entrada);
+  console.log(compra);
   entradas.push(entrada);
+  console.log(entradas);
   document.getElementById('tf').value = '';
 };
 
@@ -21,12 +27,18 @@ btnFinalizar.onclick = function() {
   document.getElementById('total').innerHTML = total; // cambia el texto de un elemento HTML
   total = 0;  // blanqueo total y compra para el proximo cliente
   compra = [];
-  let i = 0;
+  let cont = 0;
   for (let entrada of entradas) {  // el for-of para loopear por un array
-    if (entrada < 18) i++;
+    if (entrada < 18) cont++;
+    if (entrada < clienteMin) clienteMin = entrada;
+    console.log(`Cliente mas joven ${clienteMin}`);
+    if (entrada > clienteMax) clienteMax = entrada;
+    console.log(`Cliente mas viejo ${clienteMax}`);
   }
-  let porcentaje = i * 100 / entradas.length;  // regla de tres
+  let porcentaje = cont * 100 / entradas.length;  // regla de tres
   document.getElementById('menores').innerHTML = porcentaje;
-  i = 0;
-  console.log(entradas);
+  cont = 0;
+  console.log(entradas);  // DEBUG
+  document.getElementById('cliente-min').innerHTML = clienteMin;
+  document.getElementById('cliente-max').innerHTML = clienteMax;
 };
